48c67ba75a4a88044479c108720d22c6ded43020
lutwamahurbert <lutwamahurbert24@gmail.com>
2026-02-19 15:12:24 +0100

    more stuff

---
+++ complex_secpy.py
@@ -0,0 +1,11 @@
+age = 18 
+has_clearence = True
+is_citizen = True
+has_criminal_record = False
+
+if (age > 18 and has_clearence and is_citizen ) and not has_criminal_record:
+ print("✔ , you are eligible for hire")
+else:
+ print("❎, you are NOT eligibal for hire")
+
+
