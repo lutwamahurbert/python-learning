91b646cd668ca2f7b5e41110df5313aea6dc4d33
lutwamahurbert <94892649+lutwamahurbert@users.noreply.github.com>
2026-02-19 16:32:52 +0100

    Update python-files/mulit_level_auth.py with corrected indentation

--- python-files/mulit_level_auth.py
+++ python-files/mulit_level_auth.py
@@ -3,21 +3,19 @@ password = "admin1"
 two_FA_auth = "2025"
 
 if username == "admin":
- print("username correct")
-
- if password == "admin1":
-   print("password corect")
-
-  if two_FA_auth == "2025":
-   print("two_FA_auth correct")
-   print("✔ access granted")
-
-  else:
-   print("❎, incorrect 2FA-auth")
-
- else: 
-  print("❎, incorrect password")
-
+    print("username correct")
+    
+    if password == "admin1":
+        print("password corect")
+        
+        if two_FA_auth == "2025":
+            print("two_FA_auth correct")
+            print("✔ access granted")
+        
+        else:
+            print("❎, incorrect 2FA-auth")
+    
+    else: 
+        print("❎, incorrect password")
 else:
- print("incorrect username")
- 
+    print("incorrect username")
